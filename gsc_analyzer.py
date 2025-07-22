507f7d437db6872dbc8614881f906b3d1fb90997
ubseo25 <ubseo25@gmail.com>
2025-07-22 15:04:16 +0530

    Update gsc_analyzer.py

--- gsc_analyzer.py
+++ gsc_analyzer.py
@@ -4,7 +4,7 @@ from io import BytesIO
 
 st.set_page_config(page_title="📊 Search Console Analyzer ", layout="wide")
 
-st.title("📊 Google Search Console Analyzer Analyzer")
+st.title("📊 Google Search Console Analyzer")
 
 st.markdown("""
 Upload your monthly GSC data in **Excel format** with the following columns:
